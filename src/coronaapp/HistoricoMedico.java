6c55c635692734d0e63ce5dd6c9f7006fe3788c5
Anakin86708 <silva.ariel2013@gmail.com>
2020-04-24 18:38:26 -0300

    Alterado o tipo das variáveis de char para String

---
+++ src/coronaapp/HistoricoMedico.java
@@ -0,0 +1,9 @@
+package coronaapp;
+
+public class HistoricoMedico {
+	private int idHistorico;
+	private String diagnostico;
+	private String data;
+	private String medicacao;
+	private String exame;
+}
