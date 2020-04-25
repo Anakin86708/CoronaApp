3f65cae03817a632044f4ea4e3794a5be994d223
Anakin86708 <silva.ariel2013@gmail.com>
2020-04-25 18:40:16 -0300

    Getters e setters - Paloma

--- src/coronaapp/Visita.java
+++ src/coronaapp/Visita.java
@@ -3,4 +3,20 @@ package coronaapp;
 public class Visita {
 	private String data;
 	private int horario;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getHorario() {
+        return horario;
+    }
+
+    public void setHorario(int horario) {
+        this.horario = horario;
+    }
 }
