0b289881b36a5e09da6e3adbae5939c950a40f0e
Anakin86708 <silva.ariel2013@gmail.com>
2020-04-25 11:44:02 -0300

    Novos parâmetros para bairro e pais.

--- src/coronaapp/EquipeMedica.java
+++ src/coronaapp/EquipeMedica.java
@@ -5,19 +5,31 @@ public class EquipeMedica extends Pessoa {
     private String codigoLocalTrabalho;
 
     /**
-     * 
+     *
      * @param codigoLocalTrabalho
      * @param idPessoa
      * @param nome
      * @param email
      * @param cpf
      * @param telefone
+     * @param bairro
      * @param cidade
      * @param estado
-     * @param senha 
+     * @param pais
+     * @param senha
      */
-    public EquipeMedica(String codigoLocalTrabalho, int idPessoa, String nome, String email, String cpf, String telefone, String cidade, String estado, String senha) {
-        super(idPessoa, nome, email, cpf, telefone, cidade, estado, senha);
+    public EquipeMedica(String codigoLocalTrabalho, int idPessoa, String nome, String email, String cpf, String telefone, String bairro, String cidade, String estado, String pais, String senha) {
+        super(idPessoa, nome, email, cpf, telefone, bairro, cidade, estado, pais, senha);
         this.codigoLocalTrabalho = codigoLocalTrabalho;
     }
+
+    public String getCodigoLocalTrabalho() {
+        return codigoLocalTrabalho;
+    }
+
+    public void setCodigoLocalTrabalho(String codigoLocalTrabalho) {
+        this.codigoLocalTrabalho = codigoLocalTrabalho;
+    }
+    
+    
 }
