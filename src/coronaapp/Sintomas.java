79a108fca4fb8d3e333083cbecdaac9107b73be0
Anakin86708 <silva.ariel2013@gmail.com>
2020-04-25 18:40:00 -0300

    Getters e setters - Paloma

--- src/coronaapp/Sintomas.java
+++ src/coronaapp/Sintomas.java
@@ -3,4 +3,20 @@ package coronaapp;
 public class Sintomas {
 	private String nome;
 	private String gravidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGravidade() {
+        return gravidade;
+    }
+
+    public void setGravidade(String gravidade) {
+        this.gravidade = gravidade;
+    }
 }
