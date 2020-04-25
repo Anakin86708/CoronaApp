73f54a42c19a692809b0e83445e3ec44cc061577
Anakin86708 <silva.ariel2013@gmail.com>
2020-04-25 18:39:41 -0300

    Getters e setters - Paloma

--- src/coronaapp/Relatorio.java
+++ src/coronaapp/Relatorio.java
@@ -1,6 +1,23 @@
 package coronaapp;
 
 public class Relatorio {
-	private String tipo;
-	private String data;
+
+    private String tipo;
+    private String data;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
