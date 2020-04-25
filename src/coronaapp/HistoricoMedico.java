dcd8626d8f84a31f459e25703d53a0464f062213
Anakin86708 <silva.ariel2013@gmail.com>
2020-04-25 18:38:40 -0300

    Getters e setters - Paloma

--- src/coronaapp/HistoricoMedico.java
+++ src/coronaapp/HistoricoMedico.java
@@ -6,4 +6,46 @@ public class HistoricoMedico {
 	private String data;
 	private String medicacao;
 	private String exame;
+
+    public int getIdHistorico() {
+        return idHistorico;
+    }
+
+    public void setIdHistorico(int idHistorico) {
+        this.idHistorico = idHistorico;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getMedicacao() {
+        return medicacao;
+    }
+
+    public void setMedicacao(String medicacao) {
+        this.medicacao = medicacao;
+    }
+
+    public String getExame() {
+        return exame;
+    }
+
+    public void setExame(String exame) {
+        this.exame = exame;
+    }
+        
+        
 }
