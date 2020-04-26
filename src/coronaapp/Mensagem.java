a4d8395bb632287b7442419fadf511e02ec7dd51
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2020-04-26 16:29:26 -0300

    Getters e setters para mensagens

--- src/coronaapp/Mensagem.java
+++ src/coronaapp/Mensagem.java
@@ -5,4 +5,36 @@ public class Mensagem {
 	private String texto;
 	private String remetente;
 	private String destinatario;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getRemetente() {
+        return remetente;
+    }
+
+    public void setRemetente(String remetente) {
+        this.remetente = remetente;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
 }
