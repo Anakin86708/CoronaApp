78fcbf8aec49be4a54ce4eae496d37d914c52bc9
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2020-04-26 16:31:02 -0300

    Classe de sintomas possui uma lista com os sintomas

--- src/coronaapp/Sintomas.java
+++ src/coronaapp/Sintomas.java
@@ -1,15 +1,35 @@
 package coronaapp;
 
-public class Sintomas {
-	private String nome;
-	private String gravidade;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public String getNome() {
-        return nome;
+public class Sintomas {
+
+    private List<String> sintomasApresentados;
+    private String gravidade;
+
+    public Sintomas() {
+        this.sintomasApresentados = new ArrayList<>();
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public List<String> getSintomasApresentados() {
+        return sintomasApresentados;
+    }
+
+    public void addSintoma(String new_sintoma) {
+        if (!sintomasApresentados.contains(new_sintoma)) {
+            sintomasApresentados.add(new_sintoma);
+        }
+    }
+
+    public void addSintoma(String[] new_sintoma) {
+        clearSintomas();
+        sintomasApresentados.addAll(Arrays.asList(new_sintoma));
+    }
+
+    public void clearSintomas() {
+        sintomasApresentados = new ArrayList<>();
     }
 
     public String getGravidade() {
