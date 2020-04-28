5be112120d00996b229a2401f1188f5559e564d6
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2020-04-27 22:27:46 -0300

    Calcula a quantidade de acondo com o filtro

--- src/coronaapp/Relatorio.java
+++ src/coronaapp/Relatorio.java
@@ -1,23 +1,56 @@
 package coronaapp;
 
+import coronaapp.View.Login;
+import java.util.Date;
+import java.util.List;
+
 public class Relatorio {
 
-    private String tipo;
-    private String data;
+    private final String tipoFiltro;
+    private final String filtro;
+    private final Date data;
+    private int valor = 0;
 
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
+    public Relatorio(String tipoFiltro, String filtro, Date data) {
+        this.tipoFiltro = tipoFiltro;
+        this.filtro = filtro;
         this.data = data;
+        calcularValor();
     }
+
+    private void calcularValor() {
+        switch (tipoFiltro) {
+            case "Sintomas":
+             List<String> sintomasApresentados;
+                for (Paciente item : Login.pacientesInstanciados) {
+                    Sintomas itemsSintomas = item.getSintomas();
+                    sintomasApresentados = itemsSintomas.getSintomasApresentadosList();
+                    for (String sintoma : sintomasApresentados) {
+                        if (sintoma.equals(filtro)) {
+                            valor++;
+                        }
+                    }
+                }
+                break;
+            case "Regiao":
+                for (Paciente item : Login.pacientesInstanciados) {
+                    if (item.getEstado().equals(filtro)) {
+                        valor++;
+                    }
+                }
+                break;
+            case "Hospitais":
+                for (EquipeMedica item : Login.equipeMedicasInstanciados) {
+                    if (item.getCodigoLocalTrabalho().equals(filtro)) {
+                        valor++;
+                    }
+                }
+                break;
+        }
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
 }
